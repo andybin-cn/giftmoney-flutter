30aed3fefefb5678cbc31e6df82f9b85564eb6fa
jim-binea <810018715@qq.com>
2019-12-15 18:23:11 +0800

    run up

--- lib/pages/home_accessibility_page.dart
+++ lib/pages/home_accessibility_page.dart
@@ -1,3 +1,4 @@
+import 'package:advert_support/advert_support.dart';
 import 'package:flutter/material.dart';
 import 'package:giftmoney/base/base_stateful_page.dart';
 
@@ -11,8 +12,22 @@ class HomeAccessibilityPage extends BaseStatefulPage {
 class _State extends State<HomeAccessibilityPage> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-       child: Text("HomeAccessibilityPage"),
-    );
+    return Column(children: <Widget>[
+      FloatingActionButton(
+        onPressed: () async {
+          try {
+            var loadResult = await AdvertSupport.initRewardVideoAD(appID: "1110008890", posID: "4030391482552740");
+            var showResult = await AdvertSupport.showRewardVideoAD();
+            print("AdvertSupport.loadResult:$loadResult, showResult:$showResult");
+          } catch (error) {
+            print("error:$error");
+          }
+          
+        },
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
+
+      )
+    ],);
   }
 }
